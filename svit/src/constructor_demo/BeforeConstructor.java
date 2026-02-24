ae781790058eaa5a4a53541a6a2eb9bec3ad0b69
srividya <srividyam.22ds@saividya.ac.in>
2026-02-24 16:32:24 +0530

    First commit

---
+++ svit/src/constructor_demo/BeforeConstructor.java
@@ -0,0 +1,21 @@
+package constructor_demo;
+
+class Myclass
+{
+	String color;
+	int cost;
+	String Brand;
+}
+
+public class BeforeConstructor {
+			
+			public static void main(String[] args) {
+				Myclass obj=new Myclass();
+				System.out.println(obj.color + " " + obj.cost + "  " + obj.Brand);
+
+			}
+		
+
+}
+
+
---
+++ svit/src/constructor_demo/ParameterizedConst.java
@@ -0,0 +1,27 @@
+package constructor_demo;
+class Myclass2
+{
+	String color;
+	int cost;
+	String Brand;
+
+
+//Parameterized constructor
+
+Myclass2(String color,int cost,String Brand)
+{
+	this.color=color;
+	this.cost=cost;
+	this.Brand=Brand;
+}
+}
+
+public class ParameterizedConst {
+	public static void main(String[] args) {
+		Myclass2 obj=new Myclass2("white",200,"BMW");
+		Myclass2 obj1=new Myclass2("black",400,"audi");
+		System.out.println(obj.color + " " + obj.cost + "  " + obj.Brand);
+		System.out.println(obj1.color + " " + obj1.cost + "  " + obj1.Brand);
+	}
+
+}
